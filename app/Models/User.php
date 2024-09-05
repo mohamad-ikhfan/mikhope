773fe1dc6f073906fdadd26f7f9ee0c06c7e5541
mohamad-ikhfan <mohamad.ikhfan@gmail.com>
2024-09-05 15:17:51 +0700

    upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\CreatedUpdatedBy;
+use App\Traits\CreatedUpdatedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
